4f90214052cf4d349484b03a2e6315474789f0d2
Todd Qualls <tqualls@gmail.com>
2015-02-11 22:59:47 -0500

    Installs Express and creates basic index page with Angular controller.

---
+++ routes/posts.js
@@ -0,0 +1,29 @@
+/**
+ * Created by tqheel on 2/11/2015.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    var res = [
+        {
+            id: 1,
+            title: "This is post title 1",
+            body: "This is post 1 body."
+        },
+        {
+            id: 2,
+            title: "This is post title 2",
+            body: "This is post 2 body."
+        },
+        {
+            id: 3,
+            title: "This is post title 3",
+            body: "This is post 3 body."
+        }
+    ];
+
+    return res;
+});
+
+module.exports = router;
