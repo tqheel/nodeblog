16023c61626560a79cafa02116a0b65818d1c524
Todd Qualls <tqualls@gmail.com>
2015-02-16 19:13:35 -0500

    Adds content as html partials that will be used to populate the blog.

--- routes/posts.js
+++ routes/posts.js
@@ -5,25 +5,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    var res = [
-        {
-            id: 1,
-            title: "This is post title 1",
-            body: "This is post 1 body."
-        },
-        {
-            id: 2,
-            title: "This is post title 2",
-            body: "This is post 2 body."
-        },
-        {
-            id: 3,
-            title: "This is post title 3",
-            body: "This is post 3 body."
-        }
-    ];
+    //res = [
+    //    {
+    //        id: 1,
+    //        title: "This is post title 1",
+    //        body: "This is post 1 body."
+    //    },
+    //    {
+    //        id: 2,
+    //        title: "This is post title 2",
+    //        body: "This is post 2 body."
+    //    },
+    //    {
+    //        id: 3,
+    //        title: "This is post title 3",
+    //        body: "This is post 3 body."
+    //    }
+    //];
+    //
+    //return res;
 
-    return res;
 });
 
 module.exports = router;
