54524f99dce1010e00d0c190443b5d4dfdde1241
Todd Qualls <tqualls@gmail.com>
2015-02-18 17:03:14 -0500

    Implements async each method in files.js handler so that all content files are completely processed before being sent back to the router get method.

---
+++ handlers/files.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+
+module.exports = function(dir, next){
+    console.log('Resolving file path ' + dir);
+    var resolvedPath = path.resolve(dir);
+    console.log('Resolved path is ' + resolvedPath);
+    var fileContents = [];
+    fs.readdir(resolvedPath, function(err, fileList){
+        if (err) {
+            console.log('There was an error retrieving files from '+resolvedPath);
+            next(err);
+        } else {
+            console.log('Retrieved list of files from '+ resolvedPath + '...');
+            function processFile(element, callback) {
+                var pathToFile = path.join(resolvedPath, element);
+                var normPathToFile = path.normalize(pathToFile);
+                fs.readFile(normPathToFile, 'utf8', function (readErr, data) {
+                    if (readErr) {
+                        console.log(readErr);
+                        callback(readErr);
+                    }
+                    fs.stat(normPathToFile, function (statErr, stats) {
+                        if (statErr) {
+                            callback(statErr);
+                        }
+                        fileContents.push({
+                            path: normPathToFile,
+                            fileName: element,
+                            ext: path.extname(element),
+                            content: data,
+                            createDate: new Date(stats.birthtime).getDate()
+                        });
+                        console.log('Finished processing file ' + element);
+                        callback();
+                    });
+                });
+            }
+            //We have to iterate over the files asynchronously, or else the fileContents array will
+            //get sent back to the calling function before it is ready
+            var counter = 0;
+            async.each(fileList, function(fileName, callback){
+                console.log('Processing file ' + fileName);
+                processFile(fileName, function(processingErr){
+                    if(processingErr){
+                        callback(processingErr);
+                    }
+                    else{
+                        counter++;
+                        console.log(counter+' file(s) have been processed.');
+                        callback();
+                    }
+                });
+            },function(fileProcessingErr){
+                if(fileProcessingErr){
+                    console.log('A file failed tp process. Error: '+ fileProcessingErr);
+                    next(fileProcessingErr);
+                }else{
+                    if(counter===fileList.length){
+                        //files finished processing
+                        console.log('All content files have been processed.');
+                        console.log('Sending files to http router...');
+                        next(null, fileContents);
+                    }
+                }
+            });
+
+        }
+
+    });
+
+};
--- routes/posts.js
+++ routes/posts.js
@@ -3,26 +3,28 @@
  */
 var express = require('express');
 var router = express.Router();
-var getFiles = require('../modules/files.js');
+var getFiles = require('../handlers/files.js');
 var cheerio = require('cheerio');
 var _ = require('underscore');
 
 router.get('/', function(req, res, next) {
+    console.log('Getting files...');
+    getFiles('./content/published', function(err, files){
 
-    var posts = getFiles('./content/published', function(err, files){
-        console.log(files[0]);
         if(err){
             err.status = 500;
             next(err);
         }
 
         else{
+            console.log("Router has received the files.");
             var contents = [];
             //sort the file array by create date and then put in descending order (newest file first)
             files = _.sortBy(files, 'createDate').reverse();
-            //console.log(files[0].title);
+            console.log('RouterProcessing file contents.');
+            //TODO: Need to replace this iteration with async.each
             files.forEach(function(file, index){
-                console.log(file.createDate);
+                console.log(file.fileName);
                 var $ = cheerio.load(file.content);
                 var content = {
                     title: $('#title').text(),
