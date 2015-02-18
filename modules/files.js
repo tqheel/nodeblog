1125d992626286e8d50042453e9942f4fdf3b297
Todd Qualls <tqualls@gmail.com>
2015-02-17 21:20:05 -0500

    Gets loading of static files working in module, but still have async problems with forEach in files.js.

--- modules/files.js
+++ modules/files.js
@@ -2,26 +2,45 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(dir, callback){
-    fs.readdir(path.normalize(dir), function(err, list){
+    var resolvedPath = path.resolve(dir);
+    var files = [];
+    fs.readdir(resolvedPath, function(err, list){
+
         if (err) {
             callback(err);
         } else {
-            var files = [];
-            list.forEach(function (element) {
-                var stats = fs.stat(element);
-                var stream = fs.createReadStream(element);
-                var content = '';
-                stream.pipe(content);
-                var file = {
-                    path: path.normalize(element),
-                    fileName: path.basename(element),
-                    ext: path.extname(element),
-                    content: content,
-                    createDate: new Date(stats.birthtime).getDate()
-                };
-                files.push(file);
+
+            list.forEach(function (element, index, array) {
+                var pathToFile = path.join(resolvedPath, element);
+                var normPathToFile = path.normalize(pathToFile);
+                fs.readFile(normPathToFile, 'utf8', function(readErr, data){
+                    if(readErr){
+                        console.log(readErr);
+                        callback(readErr);
+                    }
+                    fs.stat(normPathToFile, function(statErr, stats){
+                        if(statErr){
+                            callback(statErr);
+                        }
+                        files.push({
+                            path: normPathToFile,
+                            fileName: element,
+                            ext: path.extname(element),
+                            content: data,
+                            createDate: new Date(stats.birthtime).getDate()
+                        });
+                    });
+                    //make sure that files are finished being processed before running callback
+                    if(index === array.length - 1){
+                        callback(null, files);
+                        console.log('Number of files processed: ' +  files.length)
+                    }
+                });
+
             });
-            callback(null, files);
+
         }
+
     });
+
 };
--- routes/posts.js
+++ routes/posts.js
@@ -9,16 +9,20 @@ var _ = require('underscore');
 
 router.get('/', function(req, res, next) {
 
-    var posts = getFiles('/content/published', function(err, files){
+    var posts = getFiles('./content/published', function(err, files){
+        console.log(files[0]);
         if(err){
             err.status = 500;
             next(err);
         }
+
         else{
             var contents = [];
             //sort the file array by create date and then put in descending order (newest file first)
             files = _.sortBy(files, 'createDate').reverse();
+            //console.log(files[0].title);
             files.forEach(function(file, index){
+                console.log(file.createDate);
                 var $ = cheerio.load(file.content);
                 var content = {
                     title: $('#title').text(),
@@ -26,9 +30,11 @@ router.get('/', function(req, res, next) {
                     fileUnixCreateDate: file.createDate,
                     body: file.content
                 };
+                console.log(content.title);
                 contents.push(content);
             });
             res = JSON.stringify(contents);
+            console.log(contents);
             next(res);
         }
     });
